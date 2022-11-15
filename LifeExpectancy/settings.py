3459b9e925cd2dd6806fd913323a2d56e8b76b85
Nagidi Rahul Varma <rahul.varma.nagidi+github@gmail.com>
2022-11-15 23:25:38 +0530

    updated settings file

--- LifeExpectancy/settings.py
+++ LifeExpectancy/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pcvb0o-@o1#@ph2*9w6q1lbqpg2jjr**-6gl+==ger=-d0%*1h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
